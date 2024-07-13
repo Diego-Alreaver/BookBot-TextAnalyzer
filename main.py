c2d30e746c296344f6d15fbc6b8d391e5756da7d
Diego-Alreaver <diego8558@hotmail.com>
2024-07-13 11:32:33 -0400

    Se agregó un contador de caracteres totales en el libro

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+
+def characters_counter(palabras):
+    character_counts = {
+
+    }
+    for char in palabras:
+        if char in character_counts:
+            character_counts[char] += 1
+        else:
+            character_counts[char] = 1
+    
+    return character_counts
+
+
+
 def main():
     # Ruta relativa al archivo
     path_to_file = "books/frankenstein.txt"
@@ -6,11 +21,19 @@ def main():
     with open(path_to_file, 'r') as f:
         file_contents = f.read()
     contador = 0
+    listalower = []
+
     # Imprimir el contenido del archivo
     word_counts = file_contents.split()
+
     for words in word_counts:
+        listalower.append(words.lower())
         contador = contador +1
     print(contador)
+
+    character_counts = characters_counter(file_contents.lower())
+    print(character_counts)
+    
     
 
 # Llamar a la función main
